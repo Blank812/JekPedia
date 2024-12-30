62602f5e3153200eec713afd689e9a14246657f8
Blank812 <ethansulaiman@gmail.com>
2024-12-30 22:31:28 +0700

    implemented command design pattern

--- JekPedia/src/main/Main.java
+++ JekPedia/src/main/Main.java
@@ -26,68 +26,77 @@ public class Main {
 			
 			switch (option) {
 			case "1":
-				String name = "";
-				do {
-					System.out.print("Input name [cannot be empty]: ");
-					name = scan.nextLine();
-				} while(name.isEmpty());
-				
-				int jobExperience = 0;
-				do {
-					System.out.print("Input job experience [> 1]: ");
-					try {
-						jobExperience = scan.nextInt();
-					} catch (Exception e) {	
-					} finally {
-						scan.nextLine();
-					}
-				} while(jobExperience <= 1);
-				
-				String jobPreferences;
-				do {
-					System.out.print("Input job preferences [Backend Dev | Frontend Dev][case sensitive]: ");
-					jobPreferences = scan.nextLine();
-				} while(!jobPreferences.equals("Backend Dev") && !jobPreferences.equals("Frontend Dev"));
-				
-				
-				
-				database.add(applicantFactory.createApplicant(name, jobExperience, jobPreferences));
-				System.out.println("Data submitted!");
+				registerApplicant();
 				break;
 
 			case "2":
-				if (database.isEmpty()) {
-					System.out.println("No data submitted yet");
-				} else {
-					for (Applicant applicant : database) {
-						applicant.displayInformation();
-						String choice = "";
-						do {
-							System.out.println("Preview next applicant form ? [N for next | U to update applicant status | Q to quit][case sensitive]: ");
-							choice = scan.nextLine();
-						} while (!choice.equals("N") && !choice.equals("Q"));
-						
-						switch (choice) {
-						case "N":
-							break;
-							
-						case "U":
-							applicant.changeState();
-							System.out.println("Applicant status updated");
-							break;
-							
-						case "Q":
-							return;
-						}
-					}
-				}
+				viewApplicants();
 				break;
 			}
 		} while (!option.equals("3"));
 		
 		System.out.println("Thank you for your cooperation!");
 		
+		
+		
 }
+	private void registerApplicant() {
+		String name = "";
+		do {
+			System.out.print("Input name [cannot be empty]: ");
+			name = scan.nextLine();
+		} while(name.isEmpty());
+		
+		int jobExperience = 0;
+		do {
+			System.out.print("Input job experience [> 1]: ");
+			try {
+				jobExperience = scan.nextInt();
+			} catch (Exception e) {	
+			} finally {
+				scan.nextLine();
+			}
+		} while(jobExperience <= 1);
+		
+		String jobPreferences;
+		do {
+			System.out.print("Input job preferences [Backend Dev | Frontend Dev][case sensitive]: ");
+			jobPreferences = scan.nextLine();
+		} while(!jobPreferences.equals("Backend Dev") && !jobPreferences.equals("Frontend Dev"));
+		
+		
+		
+		database.add(applicantFactory.createApplicant(name, jobExperience, jobPreferences));
+		System.out.println("Data submitted!");
+	}
+	
+	private void viewApplicants() {
+		if (database.isEmpty()) {
+			System.out.println("No data submitted yet");
+		} else {
+			for (Applicant applicant : database) {
+				applicant.displayInformation();
+				String choice = "";
+				do {
+					System.out.println("Preview next applicant form ? [N for next | U to update applicant status | Q to quit][case sensitive]: ");
+					choice = scan.nextLine();
+				} while (!choice.equals("N") && !choice.equals("Q"));
+				
+				switch (choice) {
+				case "N":
+					break;
+					
+				case "U":
+					applicant.changeState();
+					System.out.println("Applicant status updated");
+					break;
+					
+				case "Q":
+					return;
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
